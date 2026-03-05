56560f2dc9dab438757bee0ad18b702897dd8edd
mikesiscoe3-hash <mikesiscoe3@gmail.com>
2026-03-05 14:01:27 -0500

    Refactor to use getSupabaseAdmin for initialization

--- app/api/project-files/route.ts
+++ app/api/project-files/route.ts
@@ -1,13 +1,13 @@
  import { NextResponse } from 'next/server'
--import { supabaseAdmin, SINGLE_USER_ID } from '../../../lib/supabaseAdmin'
-+import { getSupabaseAdmin } from '../../../lib/supabaseAdmin'
+import { NextResponse } from 'next/server'
+import { supabaseAdmin, SINGLE_USER_ID } from '../../../lib/supabaseAdmin'
 
  export async function GET() {
--  const { data, error } = await supabaseAdmin
-+  const supabaseAdmin = getSupabaseAdmin()
-+  // keep your existing query below; just make sure it starts from supabaseAdmin
-+  // example:
-+  // const { data, error } = await supabaseAdmin.from('projects').select('*')
+const { data, error } = await supabaseAdmin
+  const supabaseAdmin = getSupabaseAdmin()
+  // keep your existing query below; just make sure it starts from supabaseAdmin
+  // example:
+  // const { data, error } = await supabaseAdmin.from('projects').select('*')
  
    // ...rest of your handler
  }
